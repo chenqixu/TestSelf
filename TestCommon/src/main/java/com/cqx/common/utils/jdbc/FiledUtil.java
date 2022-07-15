fb0fcce60ea47fc41e35bee8441c9aac22554034
chenqixu <13509323824@139.com>
2022-07-15 23:23:04 +0800

    [TestCommon]字段工具说明

--- TestCommon/src/main/java/com/cqx/common/utils/jdbc/FiledUtil.java
+++ TestCommon/src/main/java/com/cqx/common/utils/jdbc/FiledUtil.java
@@ -12,6 +12,32 @@ public class FiledUtil {
     private List<String> vals = new ArrayList<>();
     private StringBuilder stringBuilder = new StringBuilder();
 
+    /**
+     * <H2>添加数据</H2><br>
+     * <pre>
+     *      ################################
+     *      #oracle
+     *      ################################
+     *      FiledUtil insert_values = new FiledUtil();
+     *      insert_values.add(0, 4, "'task_001'");
+     *      insert_values.add(1, 4, "'fuzhou'");
+     *      insert_values.add(2, 4, "to_date('2022-07-15 09:49:00','yyyy-MM-dd hh24:mi:ss')");
+     *      insert_values.add(3, 4, "1");
+     *
+     *      ################################
+     *      #adb
+     *      ################################
+     *      FiledUtil insert_values = new FiledUtil();
+     *      insert_values.add(0, 4, "'task_001'");
+     *      insert_values.add(1, 4, "'fuzhou'");
+     *      insert_values.add(2, 4, "'2022-07-15 09:49:00'");
+     *      insert_values.add(3, 4, "1");
+     * </pre>
+     *
+     * @param current 当前index，从0开始，自增1
+     * @param allSize 总大小
+     * @param value   数据的值，varchar类型需要加上''，时间字段视对应的数据库而定，有的数据库支持''，有的数据库必须to_date或to_timestamp
+     */
     public void add(int current, int allSize, String value) {
         vals.add(value);
         stringBuilder.append(value);
