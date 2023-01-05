e01e2a4206d101f70eddaf5085198066be580bdb
chenqixu <13509323824@139.com>
2023-01-05 14:40:21 +0800

    [TestCommon]邮件发送工具(smtp)，增加了139邮箱的测试代码

--- TestCommon/src/test/java/com/cqx/common/utils/email/EmailUtilTest.java
+++ TestCommon/src/test/java/com/cqx/common/utils/email/EmailUtilTest.java
@@ -34,4 +34,28 @@ public class EmailUtilTest {
                 , Stream.of(receive, receive), "test-sub", "test-123"
                 , Stream.of("d:\\tmp\\1.txt", "d:\\tmp\\2.txt"));
     }
+
+    @Test
+    public void send139MailNoSSL() throws Exception {
+        // 从JVM参数中获取，使用方式：-Demail.password=xxx
+        String password = System.getProperty("email.password");
+        if (password == null || password.length() == 0) throw new NullPointerException("密码为空，请设置-Demail.password=xxx");
+        EmailUtil emailUtil = new EmailUtil(true);
+        EmailServerBean serverBean = new EmailServerBean("smtp.139.com", "25", "smtp", false);
+        EmailUserBean send = new EmailUserBean("13509323824@139.com", password);
+        EmailUserBean receive = new EmailUserBean("13509323824@139.com");
+        emailUtil.sendMail(serverBean, send, Stream.of(receive), "test-sub", "test-123");
+    }
+
+    @Test
+    public void send139MailSSL() throws Exception {
+        // 从JVM参数中获取，使用方式：-Demail.password=xxx
+        String password = System.getProperty("email.password");
+        if (password == null || password.length() == 0) throw new NullPointerException("密码为空，请设置-Demail.password=xxx");
+        EmailUtil emailUtil = new EmailUtil(true);
+        EmailServerBean serverBean = new EmailServerBean("smtp.139.com", "465", "smtp");
+        EmailUserBean send = new EmailUserBean("13509323824@139.com", password);
+        EmailUserBean receive = new EmailUserBean("13509323824@139.com");
+        emailUtil.sendMail(serverBean, send, Stream.of(receive), "test-sub", "test-123");
+    }
 }
