785e31150a2a696fa0c81958e4962c002973af12
chenqixu <13509323924@139.com>
2024-11-05 14:09:51 +0800

    [TestCommon]Map转Json测试

--- TestCommon/src/test/java/com/cqx/common/utils/string/JsonTest.java
+++ TestCommon/src/test/java/com/cqx/common/utils/string/JsonTest.java
@@ -3,6 +3,9 @@ package com.cqx.common.utils.string;
 import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * JsonTest
  *
@@ -34,5 +37,15 @@ public class JsonTest {
         JsonBean js = JSON.parseObject(str, JsonBean.class);
         System.out.println(js.getId());
         System.out.println(js.getName());
+    }
+
+    @Test
+    public void MapToJson() {
+        Map<String, String> map = new HashMap<>();
+        map.put("teamid", "123");
+        System.out.println(JSON.toJSONString(map));// {"teamid":"123"}
+        String jsonStr = "{\"teamid\":\"123\"}";
+        Map<String, String> jsonMap = JSON.parseObject(jsonStr, Map.class);
+        System.out.println(jsonMap);
     }
 }
