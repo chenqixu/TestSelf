94fc7f2f4880a822979ed23d625cd46f2f008b4c
chenqixu <13509323924@139.com>
2025-05-29 17:26:42 +0800

    [TestWork]Url.decode测试

--- TestWork/src/main/java/com/bussiness/bi/bigdata/txt/StringTest1.java
+++ TestWork/src/main/java/com/bussiness/bi/bigdata/txt/StringTest1.java
@@ -1,10 +1,15 @@
 package com.bussiness.bi.bigdata.txt;
 
+import java.net.URLDecoder;
+
 public class StringTest1 {
 	public static void main(String[] args) {
 		String dependence_time = "'$'";
 		System.out.println(dependence_time.length());
 		dependence_time = "'$-1'";
 		System.out.println(dependence_time.length());
+
+		System.out.println(URLDecoder.decode("%E8%8E%86%E7%94%B0%E7%88%B1%E5%AE%B6%E5%85%89%E7%BD%91"));
+		System.out.println(URLDecoder.decode("莆田爱家光网"));
 	}
 }
