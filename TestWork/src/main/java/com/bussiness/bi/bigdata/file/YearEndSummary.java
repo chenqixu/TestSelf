f6e1990d5cf871e528f7730e0b2f9d45f9d86b82
chenqixu <13509323924@139.com>
2026-01-21 09:07:33 +0800

    [TestWork]年终总结-分季度处理日志

--- TestWork/src/main/java/com/bussiness/bi/bigdata/file/YearEndSummary.java
+++ TestWork/src/main/java/com/bussiness/bi/bigdata/file/YearEndSummary.java
@@ -30,83 +30,90 @@ public class YearEndSummary {
     }
 
     public void fileDeal(String path) {
-        TimeCostUtil tc = new TimeCostUtil();
-        tc.start();
         int fileCnt = 0;
         final String startStr = "#        周一:";
         final String endStr = "# 公司日志结束标志";
         List<LogFile> logFiles = new ArrayList<>();
-        FileUtil writer = new FileUtil();
-        try {
-            writer.createFile("d:\\Document\\BaiduNetdiskWorkspace\\个人日志\\work_log\\2025\\述职报告材料-日志汇总.txt");
+        FileUtil fileUtil = new FileUtil();
+        // 文件后缀是md，需要排除文件名中带有"述职"
+        for (File file : FileUtil.listFilesEndWith(path, "md", "述职")) {
+            logger.info("扫描到文件={}", file.getPath());
+            fileCnt++;
+            LogFile logFile = new LogFile();
+            logFile.setPath(file.getPath());
+            logFile.setFileName(file.getName());
+            try {
+                fileUtil.setReader(file.getPath());
+                fileUtil.read(new FileResult<String>() {
+                    AtomicBoolean startFlag = new AtomicBoolean(false);
 
-            FileUtil fileUtil = new FileUtil();
-            // 文件后缀是md，需要排除文件名中带有"述职"
-            for (File file : FileUtil.listFilesEndWith(path, "md", "述职")) {
-                logger.info("扫描到文件={}", file.getPath());
-                fileCnt++;
-                LogFile logFile = new LogFile();
-                logFile.setPath(file.getPath());
-                logFile.setFileName(file.getName());
-                try {
-                    fileUtil.setReader(file.getPath());
-                    fileUtil.read(new FileResult<String>() {
-                        AtomicBoolean startFlag = new AtomicBoolean(false);
-
-                        @Override
-                        public void run(String content) throws IOException {
-                            if (content.startsWith(startStr)) {
-                                startFlag.set(true);
-                            } else if (content.startsWith(endStr)) {
-                                startFlag.set(false);
-                            }
-                            if (startFlag.get()) {
-                                logFile.addFileContent(content);
-                            }
+                    @Override
+                    public void run(String content) throws IOException {
+                        if (content.startsWith(startStr)) {
+                            startFlag.set(true);
+                        } else if (content.startsWith(endStr)) {
+                            startFlag.set(false);
                         }
-                    });
-                } catch (Exception e) {
-                    logger.error(e.getMessage(), e);
-                } finally {
-                    fileUtil.closeRead();
-                }
-                logger.info("文件处理完成，{}", logFile);
-                logFiles.add(logFile);
+                        if (startFlag.get()) {
+                            logFile.addFileContent(content);
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            } finally {
+                fileUtil.closeRead();
             }
-            logger.info("文件个数={}", fileCnt);
+            logger.info("文件处理完成，{}", logFile);
+            logFiles.add(logFile);
+        }
+        logger.info("文件个数={}", fileCnt);
 
-            // 排序
-            Collections.sort(logFiles);
+        // 排序
+        Collections.sort(logFiles);
 
-            // 处理
-            for (LogFile _logFile : logFiles) {
-                logger.info("排序后={}，开始处理", _logFile.getFileName());
-                if (_logFile.getFileLine() > 0) {
-                    writer.write(String.format("==[%s]开始==", _logFile.getFileName()));
-                    writer.newline();
-                    for (String _content : _logFile.getFileContent()) {
-                        // 跳过"请假"关键字
-                        if (!_content.contains("请假")) {
-                            writer.write(_content);
+        // 分季度处理
+        TimeCostUtil tc = new TimeCostUtil();
+        for (int i = 1; i < 5; i++) {
+            tc.start();
+            FileUtil writer = new FileUtil();
+            String writeName = "d:\\Document\\BaiduNetdiskWorkspace\\个人日志\\work_log\\2025\\述职报告材料-日志汇总-第%s季度.txt";
+            try {
+                writer.createFile(String.format(writeName, i));
+
+                // 处理
+                for (LogFile _logFile : logFiles) {
+                    if (_logFile.getQuarter() == i) {
+                        logger.info("第{}季度，开始处理文件={}", i, _logFile.getFileName());
+                        if (_logFile.getFileLine() > 0) {
+                            writer.write(String.format("==[%s]开始==", _logFile.getFileName()));
+                            writer.newline();
+                            for (String _content : _logFile.getFileContent()) {
+                                // 跳过"请假"关键字
+                                if (!_content.contains("请假")) {
+                                    writer.write(_content);
+                                    writer.newline();
+                                }
+                            }
+                            writer.write(String.format("==[%s]结束==", _logFile.getFileName()));
                             writer.newline();
                         }
                     }
-                    writer.write(String.format("==[%s]结束==", _logFile.getFileName()));
-                    writer.newline();
                 }
+            } catch (FileNotFoundException | UnsupportedEncodingException e) {
+                logger.error(e.getMessage(), e);
+            } finally {
+                writer.closeWrite();
+                logger.info("处理完成，总耗时{} ms", tc.stopAndGet());
             }
-        } catch (FileNotFoundException | UnsupportedEncodingException e) {
-            logger.error(e.getMessage(), e);
-        } finally {
-            writer.closeWrite();
-            logger.info("处理完成，总耗时{} ms", tc.stopAndGet());
         }
     }
 
     class LogFile implements Comparable<LogFile> {
         String path;
         String fileName;
-        int nameName;
+        int nameNum;
+        int quarter;
         List<String> fileContent = new ArrayList<>();
 
         @Override
@@ -115,7 +122,7 @@ public class YearEndSummary {
         }
 
         public int getNameNum() {
-            return this.nameName;
+            return this.nameNum;
         }
 
         public String getFileName() {
@@ -133,7 +140,8 @@ public class YearEndSummary {
             tmp = tmp.replace("三", "3");
             tmp = tmp.replace("四", "4");
             tmp = tmp.replace("五", "5");
-            this.nameName = Integer.valueOf(tmp);
+            this.quarter = (Integer.valueOf(tmp.substring(4, 6)) - 1) / 3 + 1;
+            this.nameNum = Integer.valueOf(tmp);
         }
 
         public List<String> getFileContent() {
@@ -154,6 +162,10 @@ public class YearEndSummary {
 
         public void setPath(String path) {
             this.path = path;
+        }
+
+        public int getQuarter() {
+            return quarter;
         }
 
         /**
